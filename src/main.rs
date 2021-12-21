bfa804c649892571935626bd1bc26a21becdeb9a
simplyme <heyitssimplyme@users.noreply.github.com>
2021-12-20 22:07:42 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ use std::{env, time::Duration};
 use futures::{stream, StreamExt};
 use serde::Deserialize;
 
-const CONCURRENT_REQUESTS: usize = 50;
-
 #[derive(Debug, Deserialize)]
 #[serde(rename_all="PascalCase")]
 struct DominosResponse {
@@ -19,37 +17,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .timeout(Duration::from_secs(2))
     .build()?;
 
-  let range = 1000..10000;
-
+  let concurrency: usize = match env::var("CONCURRENCY") {
+    Ok(val) => val.parse().expect("invalid concurrency value"),
+    _ => 50
+  };
   let store_id = env::var("STORE_ID").expect("no store id environment variable");
 
-  let responses = stream::iter(range.into_iter())
+  let responses = stream::iter(1000..10000)
     .map(|i| {
       let client = &client;
       let url = format!("https://order.dominos.ca/power/store/{}/coupon/{}?lang=en", store_id, i);
       async move {
-        let resp = client.get(url)
+        client.get(url)
           .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/11.1.2 Safari/605.1.15")
           .header("Referer", "https://order.dominos.ca/assets/build/xdomain/proxy.html")
           .send()
-          .await?;
-          // let resp = client.get(url).send().await?;
-        resp.json::<DominosResponse>().await
+          .await?
+          .json::<DominosResponse>()
+          .await
       }
     })
-    .buffer_unordered(CONCURRENT_REQUESTS);
+    .buffer_unordered(concurrency);
 
+  println!("{: <4} | {: <7} | Name", "Code", "Price");
   responses
-    .for_each(|b| async {
-      match b {
-        Ok(d) => {
-          if d.status != -404 {
-            let mut price = d.price.unwrap();
-            if price.len() == 0 { price = "N/A".to_string(); }
-            println!("Code: {} | Price: ${} | Name: {}", d.code.unwrap(), price, d.name.unwrap());
-          }
-        },
-        _ => {},
+    .for_each(|resp| async {
+      if let Ok(data) = resp {
+        if data.status != -404 {
+          let mut price = data.price.unwrap();
+          if price.is_empty() { price = "N/A".to_string(); }
+          println!("{: <4} | {: <7} | {}", data.code.unwrap(), format!("${}", price), data.name.unwrap());
+        }
       }
     })
     .await;
